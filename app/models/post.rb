11900ef5b814e6b0746a3340c90dc20bcbfb0013
Regis Martin <regis.martinravel@gmail.com>
2015-02-16 21:09:04 +0100

    Ajout de method_missing et respond_to_missing? dans le modèle post, passage à spring 1.3.2 dans Gemfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ActiveRecord::Base
     validates :titre, :presence => true
     # validates :corps, :inclusion => { :in => ["Regis", "Bidette"]}
     has_many :commentaires
+    
+    def self.method_missing(name, *args, &block)
+        if name =~ /\Aquery_by_(.+)\z/
+            where($1 => args[0]).first
+        else
+            super
+        end
+    end
+    
+    def self.respond_to_missing?(name, include_all=false)
+        name.to_s.start_with?("query_by_") || super
+    end
+    
 end
